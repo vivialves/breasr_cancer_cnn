188b83c06d255df651a92b326a9b7ffd423bbfe2
Viviane <vivialves.v@gmail.com>
2024-11-10 13:29:27 -0500

    update streamlit

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,11 @@ import keras
 from PIL import Image
 import numpy as np
 import io
+import tensorflow as tf
+
+from tensorflow.keras.preprocessing.image import img_to_array
+
+
 
 def main():
     model = keras.models.load_model("models/breast_cancer_classification-sa.h5")
@@ -11,30 +16,35 @@ def main():
     uploaded_file = st.file_uploader("Upload your image", type=["jpg", "png"])
     if uploaded_file is not None:
         data = uploaded_file.getvalue()
-        img = Image.open(io.BytesIO(data)).resize((28, 28))
-        st.image(img)
-
-        img = load_img(img_path, target_size=(224, 224))
-
-        x = img_to_array(img)
-        x = x / 255.0  
-        x = np.expand_dims(x, axis=0)  
-
-        preds = model.predict(x)
+        image = Image.open(io.BytesIO(data))
+        small_image = image.resize((112, 112))
+        st.image(small_image)
+        if image.mode != "RGB":
+            image = image.convert("RGB")     
+        image_array = img_to_array(image)
+        # if image_array.ndim == 2:
+            # image_array = np.stack((image_array,) * 3, axis=-1)
+        image_array = tf.image.resize(image_array, size=[224, 224])
+        # if image_array.dtype != 'float32':
+            # image_array = image_array.astype('float32')
+        image_array = image_array / 255.0  
+        image_array = np.expand_dims(image_array, axis=0)  
+        preds = model.predict(image_array)
         predicted_class_index = np.argmax(preds[0])
-
-        class_labels = classes_  
+        class_labels = ['Density 1 Benign',
+                        'Density 1 Malignant',
+                        'Density 2 Benign',
+                        'Density 2 Malignant',
+                        'Density 3 Benign',
+                        'Density 3 Malignant',
+                        'Density 4 Benign',
+                        'Density 4 Malignant']
         predicted_class_label = class_labels[predicted_class_index]
 
         print("Predicted class:", predicted_class_label)
 
-        arr = np.array(img)
-        
-        # arr = np.average(arr, axis=-1)
-        arr = arr.reshape(1, 28, 28, 1)
-        prediction = model.predict(arr)
-        prediction = np.argmax(prediction)
-        st.subheader(prediction)
+        st.subheader("Predicted class:")
+        st.text(predicted_class_label)
         
 if __name__ == "__main__":
     main()
